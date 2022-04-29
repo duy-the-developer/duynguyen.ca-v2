3b031e201588433595f1ff3ea190eb013e6dd21a
duy-devsense <duy.nguyen@ambigosolutions.com>
2022-04-29 01:36:26 -0400

    update heading color

--- src/components/TerminalMessage.js
+++ src/components/TerminalMessage.js
@@ -50,12 +50,12 @@ const StyledMessage = styled.div`
   }
 
   &.warning {
-    background-color: var(--color-amber-light);
+    background-color: var(--color-amber-dark);
     color: var(--color-gray-dark);
     text-shadow: 0 0 5px var(--color-gray-dark);
     display: inline-block;
     /* width: fit-content; */
-    box-shadow: 0 0 5px var(--color-amber-light);
+    box-shadow: 0 0 5px var(--color-amber-dark);
   }
 
   &.ascii {
