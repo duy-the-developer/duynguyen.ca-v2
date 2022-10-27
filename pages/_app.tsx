565d424c70be48790de52b192c962343a9868c53
duy-devsense <duy.nguyen@ambigosolutions.com>
2022-10-27 16:30:30 -0400

    fix clock update issue

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { WindowsProvider } from '../contexts/windows-context'
+
+import '../styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WindowsProvider>
+      <Component {...pageProps} />
+    </WindowsProvider>
+  )
 }
