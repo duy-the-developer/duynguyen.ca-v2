44bab1a32e9e5142572ad458fcf69c8e04768588
duy-the-developer <duythedeveloper@gmail.com>
2022-10-28 01:21:28 -0400

    add windows context

--- components/taskbar/start-button/index.tsx
+++ components/taskbar/start-button/index.tsx
@@ -1,4 +1,3 @@
-// REACT-ICONS
 import TabArrow from '../tab-arrow'
 
 const StartButton = () => {
@@ -6,13 +5,13 @@ const StartButton = () => {
     <div className='flex items-center'>
       <div className='flex bg-blue pr-4'>
         <div className='flex bg-aqua pr-4'>
-          <button className='bg-dorange px-4 font-bold text-fg dark:bg-orange'>
+          <button className='bg-dorange px-4 font-bold dark:bg-orange'>
             START
           </button>
           <TabArrow style='border-l-dorange dark:border-l-orange' />
           README.txt
         </div>
-        <div className='border-transparent border-solid border-[12px] border-l-aqua '></div>
+        <TabArrow style='border-l-aqua' />
         about.txt
       </div>
       <TabArrow style='border-l-blue' />
