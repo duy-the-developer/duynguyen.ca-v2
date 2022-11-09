83859f6b7e9761ec011fd1e09d6bf3fb273c260e
Ngoc Duy Anh Nguyen <duythedeveloper@gmail.com>
2022-11-09 00:50:21 -0500

    restructure folder for the 3rd fucking time

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import TaskBar from '../src/components/Desktop/TaskBar'
+import TaskBar from './Desktop/TaskBar'
 
 export default function RootLayout({
   children,
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import '../styles/globals.css'
 
-import Desktop from '../src/components/Desktop'
-import TaskBar from '../src/components/Desktop/TaskBar'
+import Desktop from './Desktop'
+import TaskBar from './Desktop/TaskBar'
 
 // data
-import getAllShortcuts from './fetches/getAllShortcuts'
+import useGetAllShortcuts from './hooks/useGetAllShortcuts'
 
 export default function App() {
-  const allShortCuts = getAllShortcuts('type')
+  const allShortCuts = useGetAllShortcuts('type')
   return (
     <div className='dark h-screen w-screen max-w-[100vw] max-h-[100vh]'>
       <Desktop allShortcuts={allShortCuts} />
